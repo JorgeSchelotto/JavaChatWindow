361a185847bf5858a16a4dd687c9d3db1ece8a4f
Jorge Schelotto <jasfotografo@hotmail.com>
2020-08-10 21:49:10 -0300

    First Commit

---
+++ NotePad/src/Notepad.java
@@ -0,0 +1,11 @@
+import ar.com.EduIt.Notepad.Ventana.Window;
+
+public class Notepad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Window wind = new Window();	
+		wind.setVisible(true);
+	}
+
+}
---
+++ NotePad/src/ar/com/EduIt/Notepad/Ventana/Window.java
@@ -0,0 +1,11 @@
+package ar.com.EduIt.Notepad.Ventana;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+		public Window()
+		{
+			setSize(1250,660);
+		}
+}
